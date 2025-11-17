2b9ec87159a9d9d62b1cf9e7e10b5df86544416b
_Mark <markopsec.dev.main@gmail.com>
2025-11-17 23:39:18 +0800

    some more minor fixes

--- src/1_Pages/4_contact/contact.jsx
+++ src/1_Pages/4_contact/contact.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import NavBar from "../../2_Components/1_navbar/navbar.jsx";
+import styles from "./contact.module.css"; // optional CSS module
 
-function Contact() {
+export default function Contact() {
 	return (
 		<>
-			<title>Contact Me - Portfolio</title>
+			<title>Tech Picks - Portfolio</title>
 			<NavBar />
+
+			<div className={styles.container}>
+				<div className={styles.card}>
+					<h1>🚧 Page in Progress 🚧</h1>
+					<p>We are working on this page. Please come back later!</p>
+				</div>
+			</div>
 		</>
 	);
 }
-
-export default Contact;
