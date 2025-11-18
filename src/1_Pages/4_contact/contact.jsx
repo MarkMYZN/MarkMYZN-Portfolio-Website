cc3e7cf86d74adf115cced257c0588b0eab292da
MarkMYZN <markopsec.dev.main@gmail.com>
2025-11-19 01:21:39 +0800

    added contact page ui

--- src/1_Pages/4_contact/contact.jsx
+++ src/1_Pages/4_contact/contact.jsx
@@ -1,19 +1,78 @@
 import React from "react";
+import styles from "./contact.module.css";
+import { Link } from "react-router-dom";
 import NavBar from "../../2_Components/1_navbar/navbar.jsx";
-import styles from "./contact.module.css"; // optional CSS module
+import Card from "../../2_Components/3_Card/card.jsx";
 
 export default function Contact() {
 	return (
 		<>
-			<title>Tech Picks - Portfolio</title>
+			<title>Contact - Portfolio</title>
 			<NavBar />
 
-			<div className={styles.container}>
-				<div className={styles.card}>
-					<h1>🚧 Page in Progress 🚧</h1>
-					<p>We are working on this page. Please come back later!</p>
-				</div>
-			</div>
+			<section className={styles.sect1}>
+				<h1>You can reach me out here!</h1>
+			</section>
+			<section>
+				<h4>(You can click on each to go to each platforms!)</h4>
+			</section>
+
+			<section className={styles.contactContainer}>
+				<Link to="https://github.com/MarkMYZN">
+					<Card
+						className={styles.card}
+						image="https://www.svgrepo.com/show/448225/github.svg"
+						title="Github"
+					/>
+				</Link>
+
+				<Link to="https://web.facebook.com/Kotomi.nana.18">
+					<Card
+						className={styles.card}
+						image="https://www.svgrepo.com/show/448224/facebook.svg"
+						title="Facebook"
+					/>
+				</Link>
+
+				<Link to="mailto:markopsec.dev.main+Web_Portfolio@gmail.com">
+					<Card
+						className={styles.card}
+						image="https://www.svgrepo.com/show/452213/gmail.svg"
+						title="Gmail"
+					/>
+				</Link>
+
+				<Link to="https://www.linkedin.com/in/mark-mostrales/">
+					<Card
+						className={styles.card}
+						image="https://www.svgrepo.com/show/448234/linkedin.svg"
+						title="LinkedIn"
+					/>
+				</Link>
+
+				<Link to="https://www.reddit.com/user/MarkMYZN">
+					<Card
+						className={styles.card}
+						image="https://www.svgrepo.com/show/452094/reddit.svg"
+						title="Reddit"
+					/>
+				</Link>
+			</section>
 		</>
 	);
 }
+
+// <h1>Github</h1>
+// <image
+// 	src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+// 	className={styles.imgSettings}></image>
+// <h1>Facebook</h1>
+// <image
+// 	src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/facebook/facebook-original.svg"
+// 	className={styles.imgSettings}></image>
+// <h1>Gmail</h1>
+// <image src="https://www.svgrepo.com/show/452213/gmail.svg" className={styles.imgSettings}></image>
+// <h1>LinkedIn</h1>
+// <image
+// 	src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg"
+// 	className={styles.imgSettings}></image>
